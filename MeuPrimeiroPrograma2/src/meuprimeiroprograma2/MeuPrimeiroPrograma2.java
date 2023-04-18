94f05d1e2a17b1cb7b397465ef77e6e01d814014
dedojs <xandresousax@gmail.com>
2023-04-18 16:11:42 -0300

    iniciando curso

---
+++ MeuPrimeiroPrograma2/src/meuprimeiroprograma2/MeuPrimeiroPrograma2.java
@@ -0,0 +1,29 @@
+package meuprimeiroprograma2;
+
+import java.util.Scanner;
+
+public class MeuPrimeiroPrograma2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Meu primeiro programa 2");
+        System.out.println("Aula 2 - Gerando comentários");
+        // tipo de comentário 1
+        /*
+            tipo de comentário 2
+        */
+        /**
+         * Comentário de Documentação
+        */
+        
+        System.out.println("Aula 3");
+        Scanner leitor = new Scanner(System.in);
+        int idade = leitor.nextInt();
+        float cotacaoDolar = leitor.nextFloat();
+        String nome = leitor.nextLine();
+        String texto = leitor.next();
+        
+        System.out.println("A idade é: idade");
+    }
+    
+}
